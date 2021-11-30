bc993feb4cbb73383ddfb9f00798e55b4314687f
marcoescorza117 <marcoescorza117@gmail.com>
2021-11-30 16:47:50 -0600

    SP modificados para testing

---
+++ 5.-DatosCurriculares/2.-Formación Adicional/sps/spTerceraSeccion/1.-rhcUPI_DatosCurriculum_Formacion_DocumentoAgregarModificar.sql
@@ -0,0 +1,61 @@
+CREATE PROCEDURE [dbo].[rhcUPI_DatosCurriculum_Formacion_DocumentoAgregarModificar]
+	 @idCurriculumFormacion INT 
+	,@idDocumento BIGINT 
+
+	,@usuarioRealizo VARCHAR (60)
+	,@dispositivo VARCHAR (150)
+	,@direccionIP VARCHAR (40)
+	,@app VARCHAR (50)
+
+	,@idCurriculumFormacionDocumento BIGINT OUTPUT
+	,@noError INT OUTPUT 
+	,@mensaje VARCHAR(255) OUTPUT
+
+AS
+BEGIN
+
+    EXECUTE [dbo].[rhcUPI_Curriculum_Formacion_DocumentoAgregarModificarValidar]
+         @idDocumento
+        ,@noError OUTPUT
+		,@mensaje OUTPUT
+
+    IF ISNULL(@noError,0) != 0
+	BEGIN
+		RETURN
+	END
+
+	EXECUTE [dbo].[rhcUPI_Curriculum_Formacion_DocumentoAgregarModificar]
+		 @idCurriculumFormacion
+		,@idDocumento
+		,@usuarioRealizo
+		,@idCurriculumFormacionDocumento OUTPUT  
+		,@noError OUTPUT
+		,@mensaje OUTPUT
+
+	IF ISNULL(@noError,0) != 0 
+	BEGIN
+		RETURN
+	END
+
+	DECLARE  
+		 @idBitacora BIGINT
+		,@accion VARCHAR(50)
+	SET @accion = 'DatosCurriculumFormacionDocumento.Agregar '
+
+	EXECUTE rhcUPI_Datos_BitacoraAgregar
+		 @usuarioRealizo
+		,@dispositivo
+		,@direccionIP
+		,@accion
+		,@app
+		,@idBitacora OUTPUT
+		,@noError OUTPUT
+		,@mensaje OUTPUT
+
+	IF ISNULL(@noError,0) != 0 
+	BEGIN
+		RETURN
+	END
+END
+go
+
